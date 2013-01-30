f5bfbc0805a2094fa0b9ea8ae9e432a3d97cbebe
tonypai <tony77794@gmail.com>
2013-01-30 17:48:29 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,112 @@
+var express = require('express'),
+    app = express(),
+    http = require('http'),
+    server = http.createServer(app),
+    io = require('socket.io').listen(server, {
+        log: false
+    }),
+	fs = require('fs'),
+	svrport = 666;
+
+server.listen(svrport);
+console.log("TH server listening port "+svrport);
+
+var idle_player = [];
+io.sockets.on('connection', function (socket) {
+	//when a player ask for join/leave
+	socket.on('request', function (data) {
+		var p1 = data.player;
+		var cmd = data.order;
+		if(idle_player.length > 0){
+			for(var i=0;i<idle_player.length;i++) {
+				if(p1.career != idle_player[i].career) {
+					//find someone to match them
+					if(cmd == "join") {
+						var p2 = idle_player[i];
+						idle_player.splice(i, 1);
+						socket.emit("notice", [p1, p2]);
+						socket.broadcast.emit("notice", [p1, p2]);
+						console.log(p1.username+" match "+p2.username);
+					}
+					break;
+				}
+				else if(p1.username == idle_player[i].username &&
+						p1.career == idle_player[i].career) {
+					//remove player from idle_player
+					idle_player.splice(i, 1);
+					console.log(p1.username+" left");
+				}
+				if(i == idle_player.length - 1) {
+					//push player into idle_player, if only him.
+					if(cmd == "join") {
+						idle_player.push(p1);
+						console.log(p1.username+" join");
+						socket.emit("idle");
+					}
+					break;
+				}
+			}
+		}
+		else {
+			//push player into idle_player, if no one here.
+			if(cmd == "join") {
+				idle_player.push(p1);
+				socket.emit("idle");
+			}
+		}
+		console.log(idle_player);
+    });
+
+	//pass duel message to everyone, but sender.
+	socket.on("match", function(data) {
+		socket.broadcast.emit("match", data);
+	});
+
+	//send player left message to his opponent
+	socket.on("exitGame", function(data) {
+		var p2 = data[1];
+		socket.broadcast.emit("oppLeft", {player: p2});
+	})
+	
+	//count the result, and return to player and his opponent
+	socket.on("checkpoint", function(data) {
+		var p1 = data[0];
+		var p2 = data[1];
+		var p1_point = p1.point[p1.round-1];
+		var p2_point = p2.point[p2.round-1];
+		var msg = "";
+		if(p1.career == "batter") {
+			if(p1_point > p2_point) {
+				if(p1_point - p2_point > 30) {
+					msg = "It's deep~~~~~, and gone!!!\n<br>這個球相當的深遠~~~ 是全壘打!!!";
+					msgid = 3;
+				}
+				else {
+					msg = "Base hit!\n<br>結實的打了出去, 是個漂亮的安打!";
+					msgid = 2;
+				}
+			}
+			else {
+				msg = "Strike!\n<br>揮棒落空~ 記個好球!";
+				msgid = 1;
+			}
+		}
+		socket.emit("result", {p1: p1, p2: p2, msgid: msgid, msg: msg});
+		socket.broadcast.emit("result", {p1: p1, p2: p2, msgid: msgid, msg: msg});
+	});
+});
+
+app.configure(function () {
+	app.use('/js', express.static(__dirname + '/js'));
+	app.use('/images', express.static(__dirname + '/images'));
+	app.get('/client.html', function (req, res) {
+		fs.readFile(__dirname + '/client.html', function (err, data) {
+            if (err) {
+                res.writeHead(500);
+                return res.end("Error loading client.html");
+            }
+            res.writeHead(200);
+            res.end(data);
+        });
+	});
+});
---
+++ js/funcs.js
@@ -0,0 +1,74 @@
+var checkpoint = function() {
+	socket.emit("checkpoint", [p1, p2]);
+};
+
+var setPlayerIcon = function(id) {
+	if(p1.career == "pitcher")
+		$("#player_icon").html("<font color='red'>"+p2.username+"</font><br><img src='images/player_batter"+id+".png'>");
+	else if(p1.career == "batter")
+		$("#player_icon").html("<font color='blue'>"+p2.username+"</font><br><img src='images/player_pitcher"+id+".png'>");
+};
+
+var roll_point;
+var youcanroll = function() {
+	$("#stopRolling").prop("disabled", false);
+	roll_point = setInterval(function() {
+		var point = $("#minepoint").prop("value");
+		if(point < 100) {
+			$("#minepoint").prop("value", ++point);
+		}
+		else {
+			$("#minepoint").prop("value", 0);
+		}
+	}, 5);
+};
+
+var resetScene = function() {
+
+	//score panel
+	$("#score").html("STRIKE: "+p1.score.strike+" / "+
+				"HIT: "+p1.score.hit+" / "+
+				"HOMERUN: "+p1.score.homerun);
+
+	//player icon
+	setPlayerIcon(1);
+
+	//message: ready
+	$("#msg").html("Ready!\n<br>雙方請準備!");
+	console.log("scene reset! / 場景重置!");
+
+	//pitcher settigns
+	if(p1.career == "pitcher") {
+		$("#stopRolling").prop("value", "THROW / 投出");
+		$("#stopRolling").prop("disabled", false);
+		$("#enemypoint").prop("value", 0);
+		//rolling point
+		youcanroll();
+	}
+	//batter settings
+	else if(p1.career == "batter") {
+		$("#stopRolling").prop("value", "HIT / 揮棒");
+		$("#stopRolling").prop("disabled", true);
+		$("#minepoint").prop("value", 0);
+		$("#enemypoint").prop("value", 0);
+	}
+
+	//bind click event
+	$("#stopRolling").unbind().bind("click", function() {
+		$(this).prop("disabled", true);
+		clearInterval(roll_point);
+		p1.round ++;
+		p1.point.push($("#minepoint").prop("value"));
+		if(p1.career == "pitcher") {
+			$("#msg").html("Pitcher throw!\n<br>投手投出!");
+			//send duel message to server
+			socket.emit("match", [p1, p2]);
+		}
+		else if(p1.career == "batter") {
+			checkpoint();
+		}
+	});
+	
+	$("#match_panel").hide(0);
+	$(".game_panel").show(0);
+};
